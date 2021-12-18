f71432d5c06bdecfd93fbd129854d7804b21ad03
lewkenn <95766273+lewkenn@users.noreply.github.com>
2021-12-19 00:25:21 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -386,13 +386,13 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 handler.help = ['menu', 'help', '?']
 handler.tags = ['main']
 handler.command = /^(menu|help|\?)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.premium = true
+handler.group = true
+handler.private = true
 
-handler.admin = false
+handler.admin = true
 handler.botAdmin = false
 
 handler.fail = null
